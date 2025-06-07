43fee37d55bed7b86421624a1f6bcec1f0d43ddb
Lukáš Homola <lukas-homola@seznam.cz>
2025-06-07 14:23:34 +0200

    Adjusting visuals for different viewports.
    Larger tablet - hero section, TODO: Decrease size of the project showcase element.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/* Fixed header behavior */
+window.addEventListener("scroll", function () {
+  const header = this.document.querySelector(".header_content_desktop");
+  if (this.window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+/* Projects slider */
 new Swiper(".mySwiper", {
   spaceBetween: 10,
   slidesPerView: 1,
